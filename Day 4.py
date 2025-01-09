439d0b3c8017e777e89212312459139bb855b4c6
Fayvor-clou <onokofavour2018@gmail.com>
2025-01-10 00:18:10 +0100

    Create Day 4.py
    Loops

---
+++ Day 4.py
@@ -0,0 +1,9 @@
+#Create a simple countdown program
+
+#Using a for loop for countdown from 10 to 1
+print("Countdown using a for loop:")
+
+count = 10
+
+for count in range(10, 0, -1): #Start at 10, down to 1, decrement by 1
+    print(count)
