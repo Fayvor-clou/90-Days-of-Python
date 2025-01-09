973989c64a00e8825279a165e0d600a8597d2453
Fayvor-clou <onokofavour2018@gmail.com>
2025-01-10 00:33:12 +0100

    Update Day 4.py
    Loops

--- Day 4.py
+++ Day 4.py
@@ -7,3 +7,13 @@ count = 10
 
 for count in range(10, 0, -1): #Start at 10, down to 1, decrement by 1
     print(count)
+
+#Using a while loop for countdown from 10 to 1
+print("Countdown using a while loop:")
+
+count = 10
+while count > 0:
+    print(count) #print the current number
+    count -= 1 #Decrease count by 1
+
+print("end of loop") #Message after while loop countdown
